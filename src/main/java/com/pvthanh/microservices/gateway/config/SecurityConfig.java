7452caadf3f86e048ad5ed3c2552ec038eb13795
pvthanh <pvthanh@hanatourjsv.com>
2026-07-14 11:23:37 +0700

    feat(docs): add swagger-ui, api-docs for api-gateway

--- src/main/java/com/pvthanh/microservices/gateway/config/SecurityConfig.java
+++ src/main/java/com/pvthanh/microservices/gateway/config/SecurityConfig.java
@@ -8,9 +8,13 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    private final String[] FREE_RESOURCE_URLS = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/aggregate/**"};
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(FREE_RESOURCE_URLS).permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .build();
